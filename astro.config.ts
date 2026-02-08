70e26b02f0ee55dbc714ee35d5d1a7e5b7f0b12c
raisbecka <raisbecka09@gmail.com>
2026-02-08 07:32:41 -0500

    Redeploying with https:// added to site url

--- astro.config.ts
+++ astro.config.ts
@@ -19,7 +19,7 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'raisbecka.github.io',
+  site: 'https://raisbecka.github.io',
   base: '/MyPortfolio',
   // Static output - API routes are handled by Cloudflare Pages Functions in /functions folder
   integrations: [
